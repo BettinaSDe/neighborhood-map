47706d0f72274f178d298f17283bc5ce6093e4b0
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2018-11-17 14:27:03 +0100

    prep google maps -> app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOMServer from 'react-dom/server';
+import ReactDOM from 'react-dom';
 import './App.css';
+//import *API from GoogleMaps API ?
+//import Filters ?
+//import Markers ?
+
+
+let this;
+
+let map;
+
+let markers = [];
+
+let marker;
+
+let locationState = [
+  xxxxx]
+
 
 class App extends Component {
+
+  state = {
+
+    googleMap: [],
+
+    //xxxxxx
+
+  }
+
+
+  componentDidMount() {
+     // xxxx
+
+
   render() {
+
+
+//xxxxxxx
+
+
     return (
       <div className="App">
         <header className="App-header">
+        <h1 className="App-title">Neighborhood Maps Project</h1>
+
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            //href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -21,6 +59,7 @@ class App extends Component {
           </a>
         </header>
       </div>
+       <div id='map'></div>
     );
   }
 }
